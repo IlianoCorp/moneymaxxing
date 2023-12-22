8a7298f6b156052c8c47215ec9d0b9a2d20e37b7
Ilias Mhani <Iliano.corp@gmail.com>
2023-12-22 14:13:21 +0100

    su

---
+++ components/features.tsx
@@ -0,0 +1,116 @@
+"use client";
+import star from "../svgs/star-img (1).svg";
+import { motion } from "framer-motion";
+
+export default function Features() {
+  return (
+    <main className="features">
+      <div className=" flex flex-wrap md:gap-40 justify-center mt-14 mb-20">
+        <img
+          className="opacity-90 w-[580px] h-[420px] sizecoin "
+          alt=""
+          src="/images/users.png"
+        />
+        {/*Feature 1*/}
+        <div className="flex flex-col flex-wrap items-start justify-center ">
+          <div className="absolute">
+            <img className="w-40" src={star} alt="" />
+          </div>
+          <motion.div
+            initial={{ opacity: 0, y: 15 }}
+            animate={{ y: 12 }}
+            exit={{ opacity: 0, y: 15 }}
+            transition={{ delay: 0.25 }}
+            whileInView={{ opacity: 1, y: 0 }}
+          >
+            <div
+              className="cointext text-3xl  text-transparent bg-gradient-to-t bg-clip-text dark:text-zinc-200 font-extrabold leading-tighter tracking-tighter mb-4"
+            >
+              <span className=" whitespace-nowrap">
+                <span className=" text-transparent bg-gradient-to-t bg-clip-text from-zinc-500 dark:to-white to-black">
+                  The better the{" "}
+                  <span className="bg-clip-text text-transparent bg-gradient-to-r from-red-500 to-yellow-400">
+                    strategy
+                  </span>{" "}
+                </span>
+              </span>
+              <br />
+              <span className="text-transparent bg-gradient-to-t bg-clip-text from-zinc-500 dark:to-white to-black">
+                The more{" "}
+                <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">
+                  results
+                </span>{" "}
+                <br />
+                <span>
+                  The more{" "}
+                  <span className="bg-clip-text text-transparent bg-gradient-to-r from-yellow-500 to-green-400">
+                    $$$
+                  </span>
+                </span>
+              </span>
+            </div>
+          </motion.div>
+        </div>
+      </div>
+
+      <div className="oder2 flex md:flex-wrap md:gap-40 justify-center text-31xl text-black-black-100 font-body-14">
+        {/*Feature 2*/}
+        <div className="flex flex-col items-start sm:order-2 justify-center ">
+          <motion.div
+            initial={{ opacity: 0, y: 15 }}
+            animate={{ y: 12 }}
+            exit={{ opacity: 0, y: 15 }}
+            transition={{ delay: 0.25 }}
+            whileInView={{ opacity: 1, y: 0 }}
+          >
+            <div
+              className="oder1 cointext text-3xl  text-transparent bg-gradient-to-t bg-clip-text dark:text-zinc-200 font-extrabold leading-tighter tracking-tighter mb-4"
+              data-aos="zoom-y-out"
+            >
+              <span className=" whitespace-nowrap">
+                <span className=" text-transparent bg-gradient-to-t bg-clip-text from-zinc-500 dark:to-white to-black">
+                  You heard it{" "}
+                  <span className="bg-clip-text text-transparent bg-gradient-to-r from-red-500 to-yellow-400">
+                    before
+                  </span>{" "}
+                </span>
+              </span>
+              <br />
+              <span className="text-transparent bg-gradient-to-t bg-clip-text from-zinc-500 dark:to-white to-black">
+                Invest in{" "}
+                <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">
+                  yourself
+                </span>{" "}
+                <br />
+                <span>
+                  Now is the{" "}
+                  <span className="bg-clip-text text-transparent bg-gradient-to-r from-yellow-500 to-green-400">
+                    time
+                  </span>
+                </span>
+              </span>
+            </div>
+          </motion.div>
+        </div>
+        <img
+          className="oder mr-30 opacity-90 w-[580px] h-[420px] sizecoin"
+          alt=""
+          src="/images/invest.png"
+        />
+      </div>
+      <img src="" alt="" />
+
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 1440 320"
+        className="absolute w-auto"
+      >
+        <path
+          fill="#09090B"
+          fill-opacity="1"
+          d="M0,96L48,96C96,96,192,96,288,128C384,160,480,224,576,213.3C672,203,768,117,864,96C960,75,1056,117,1152,112C1248,107,1344,53,1392,26.7L1440,0L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
+        ></path>
+      </svg>
+    </main>
+  );
+}
---
+++ components/quote.tsx
@@ -0,0 +1,121 @@
+"use client";
+import { useEffect, useState } from "react";
+import { motion, useScroll, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import Typewriter from "typewriter-effect";
+
+
+
+export default function Quote() {
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+
+  const variants = {
+    hidden: { pathLength: 0, opacity: 0 },
+    visible: (i: number) => {
+      const delay = 1 + i * 0.5;
+      return {
+        pathLength: 1,
+        opacity: 1,
+        transition: {
+          pathLength: { delay, duration: 5.5, },
+          opacity: { delay, duration: 0.2 }
+        }
+      };
+    }
+};
+
+  useEffect(() => {
+    if (inView) {
+      controls.start({
+        x: 420,
+      });
+    }
+  }, [inView]);
+  return (
+    <main className="quote">
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css"
+        integrity="sha384-jLKHWM3JRmfMU0A5x5AkjWkw/EYfGUAGagvnfryNV3F9VqM98XiIH7VBGVoxVSc7"
+      />
+      <div className="flex justify-center w-auto h-[600px] text-left text-31xl text-black-black-100 font-body-14">
+        {/*Feature 1*/}
+        <div className=" top-[120px] w-[580px] flex flex-col items-start justify-center gap-[32px]">
+          <div className="deck3">
+            <motion.img
+              src="/images/brucelee.png"
+              className="absolute rounded-2xl w-[0px] top-[-3rem] lg:w-auto "
+              initial={{ opacity: 1, x: 0 }}
+              animate="variants"
+              exit={{ opacity: 0, x: 15 }}
+              transition={{pathLength: { duration: 10.5, }, delay: 0.75 }}
+              whileInView={{ opacity: 1, x: 448 }}
+            />
+
+            <div className="card3">
+              <header className="cardHeader3 pt-6">
+                <i className="fab fa-instagram cardHeader_type"></i>
+              </header>
+              <div className="cardBody3">
+                <p className="cardText3">
+                  One does not accumulate but eliminate. It is not daily
+                  increase but daily decrease. The height of cultivation always
+                  runs to simplicity.
+                  <span className="cardText_highlight3"></span> <br /> <br />{" "}
+                  <span className="cardText_highlight3">- Bruce Lee</span>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="window flex flex-wrap justify-center lg:justify-between md:justify-center sm:justify-between ">
+        <div className="textL stroke pb-4 md:pl-10 highlighted-text-shadow text-center md:text-left text-2xl lg:text-6xl md:text-5xl font-extrabold leading-tighter  font-body-14 first-letter">
+          <span className=" whitespace-nowrap">
+            <span className=" text-transparent bg-gradient-to-t bg-clip-text from-zinc-500 dark:to-white to-black">
+              Let us build your{" "}
+              <span className="bg-clip-text text-transparent bg-gradient-to-r from-red-500 to-yellow-400">
+                website
+              </span>{" "}
+            </span>
+          </span>
+          <br />
+          <span className="text-transparent bg-gradient-to-t bg-clip-text from-zinc-500 dark:to-white to-black">
+            So you can focus on{" "}
+            <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">
+              What
+            </span>{" "}
+            <br />
+            <span>
+              Truely matters to{" "}
+              <span className="bg-clip-text text-transparent bg-gradient-to-r from-yellow-500 to-green-400">
+                You
+              </span>
+            </span>
+          </span>
+        </div>
+        <div className="mac md:mr-4  w-auto text-left  text-black-black-100 font-body-14">
+          <div className="mac1 outer yosemite w-[400px] h-[400px]">
+            <div className="dot red"></div>
+            <div className="dot amber"></div>
+            <div className="dot green"></div>
+            <div className="px-2 w- text-3xl text-white mt-[20%]">
+              <Typewriter
+                options={{
+                  loop: true,
+                }}
+                onInit={(typewriter) => {
+                  typewriter
+                    .typeString("See the impact, <br /> Your success, <br /> Powered by our website.")
+                    
+                    .start();
+                }}
+              /></div>
+            
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
